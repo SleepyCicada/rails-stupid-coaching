008800a04db8dcfc7727f99d46b8e92fa1772b5c
Pavel Mikhailau <paul.mikhailau@gmail.com>
2025-10-04 14:35:28 -0400

    Code for 2 pages added

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,22 @@
 class QuestionsController < ApplicationController
  def ask
  end
+
+def answer
+    input = params[:input]
+
+    if input.nil?
+      @response = "Please enter something for the coach to respond to."
+    else
+      @response =
+        if input.include?("?")
+          "Silly question, get dressed and go to work!"
+        elsif input.downcase == "i am going to work"
+          "Great!"
+        else
+          "I don't care, get dressed and go to work!"
+        end
+    end
+  end
+
 end
